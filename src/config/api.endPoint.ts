1a0c2a67de9640ee9817bcdc1c818a26dbb42849
Mayada2hmed <eng.mayada.ahmed.elnagar@gmail.com>
2026-03-12 17:30:45 +0200

    change

--- src/config/api.endPoint.ts
+++ src/config/api.endPoint.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 export const baseURL=`https://upskilling-egypt.com:3005`;
 export const imgBaseURL='';
 
@@ -13,3 +11,44 @@ export const AUTH_URLS = {
     LOGOUT : `/api/auth/logout`,    
 
 }
+// Instractor Module
+export const STUDENT = {
+  GET_ALL : `/api/student`,
+  TOP_FIVE_STUDENTS : `/api/student/top-five`,
+  GET_BY_ID : (id:string) =>`/api/student/${id}`,
+  GET_STUDENTS_WITHOUT_GROUP:`api/student/without-group`
+
+}
+export const GROUPS = {
+  GET_ALL : `/api/group`,
+  GET_BY_ID : (id:string) =>`/api/group/${id}`,
+  CREATE_GROUP : `/api/group`,
+  UPDATE_GROUP : (id:string)=>`/api/group/${id}`,
+  DELETE_GROUP : (id:string)=>`/api/group/${id}`,
+
+}
+export const QUESTION = {
+  GET_ALL : `/api/question`,
+  GET_BY_ID : (id:string) =>`/api/question/${id}`,
+  CREATE_QUESTION : `/api/question`,
+  UPDATE_QUESTION : (id:string)=>`/api/question/${id}`,
+  DELETE_QUESTION : (id:string)=>`/api/question/${id}`,
+  SEARCH : `/api/question/search`,
+}
+export const QUIZ = {
+  GET_ALL : `/api/quiz`,
+  GET_BY_ID : (id:string) =>`/api/quiz/${id}`,
+  CREATE_QUESTION : `/api/quiz`,
+  UPDATE_QUESTION : (id:string)=>`/api/quiz/${id}`,
+  DELETE_QUESTION : (id:string)=>`/api/quiz/${id}`,
+  SEARCH : `/api/question/search`,
+  JOIN: `/api/quiz/join`,
+  SUBMIT :(id:string)=>  `/api/quiz/submit/${id}`,
+  WITHOUT_ANSWER : (id:string) => `/api/quiz/without-answers/${id}`,
+  RESULT : `/api/quiz/result`,
+  FIRST_INCOMMING :`/api/quiz/incomming`,
+  LAST_COMPLETED : `/api/quiz/completed`,
+  REASSIGN : (id:string) => `/api/quiz/reassign/${id}`,
+
+}
+// end Instractor module
